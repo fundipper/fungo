8f2cbbecf4081a361804ad903bec22d007c0ea70
andy.jiang <150haomengting@163.com>
2022-04-27 16:21:08 +0800

    modify version

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,7 +3,7 @@ package cli
 import "sync"
 
 const (
-	_VERSION = "v0.2.8"
+	_VERSION = "v0.2.9"
 	_SERVER  = "server run on http://localhost%s"
 	_BANNER  = `
     ____                                
